c06f5eaccfe7618b51908287a78ea60357fd7280
Hamdallah-Abdulfatah <105557190+Hamdallah-Abdulfatah@users.noreply.github.com>
2022-10-14 23:08:02 +0100

    python datatype

---
+++ datatype.py
@@ -0,0 +1,110 @@
+from mimetypes import init
+
+
+y = "Allah the self sufficient"
+
+#display y:
+print(y)
+
+#display the data type of y:
+print (type(y))
+
+a = 24
+
+#display a:
+print(a)
+
+#display the data type of a:
+print(type(a))
+
+b = 24.8
+
+#display b:
+print(b)
+
+#display the data type of b:
+print(print(b))
+
+c = ["Soladoye", "Muttalib", "Olatunji"]
+
+#display c:
+print(c)
+
+#display the data type c:
+print(type(c))
+
+d = ("Soladoye", "Muttalib", "Olatunji")
+
+#display d:
+print(d)
+
+#display the data type d:
+print(type(d))
+
+e = range(9)
+
+#display e
+print(e)
+
+#display the data type e:
+print(type(e))
+
+f = {"Soladoye", "Muttalib", "Olatunji"}
+
+#display f
+print(f)
+
+#display the data type f:
+print(type(f))
+
+g = {"Soladoye" : "Muttalib", "Olatunji" : 94}
+
+#display g
+print(g)
+
+#display the data type g:
+print(type(g))
+
+h = frozenset({"Soladoye", "Muttalib", "Olatunji"})
+  
+#display h
+print(h)
+
+#display the data type h:
+print(type(h))
+
+i = True
+
+#display i
+print(i)
+
+#display the data type in h:
+print(type(i))
+
+j = b"Hello"
+
+#display j
+print(j)
+
+#display the data type in j:
+print(type(j))
+
+k = bytearray(6)
+
+#display k:
+print(k)
+
+#display the data type in k:
+print(type(k))
+
+l = None
+
+#display l
+print(l)
+
+#display the data type in l
+print(type(l)) 
+
+
+
+
