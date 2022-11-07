8618e017c450ea069a03b16f6e03de9f36c1cda6
Hamdallah-Abdulfatah <105557190+Hamdallah-Abdulfatah@users.noreply.github.com>
2022-11-07 09:23:53 -0800

    python casting

---
+++ python casting.py
@@ -0,0 +1,17 @@
+# Integer
+x = int(4.8)
+y = int("6")
+print(x)
+print(y)
+
+#floats
+a = float(4)
+b = float("9")
+print(a)
+print(b)
+
+#strings
+c = str(8)
+d = str(7.4)
+print(c)
+print(d)
---
+++ python strings.py
@@ -0,0 +1,43 @@
+# Strings
+print("Hamdallah Abdulfatah")
+print('Hamdallah Abdulfatah')
+
+# Assign string to a variable
+h = "Hamdallah Abdulfatah"
+print(h)
+
+# Multiline Strings
+z = """ According to HATTLE (2008),
+th ideal learning sitution is when the teachers
+and learners are seeking to these question"""
+
+# String are Array
+k = "Hamdallah Abdulfatah"
+print(k[4])
+
+# Looping through a String
+for x in "beautiful":
+    print(x)
+ 
+ #Check string
+#check if "interpreter" is present in the following text:
+txt = "No Python interpreter is selected"
+print("interpreter" in txt)
+
+# Use it in an if statement:
+#print only if "interpreter" is present:
+if "interpreter" in txt:
+    print ("Yes, 'free in present.")
+
+#Check if NOT
+#check if "expensive" is not present in the following text:
+txt = "No Python interpreter is selected"
+print ("expensive" not in txt)
+
+#Use it in an if statement:
+txt = "No Python interpreter is selected"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+    
+
+
